a13085d3d83823509dde462f3faa986ad145c105
bijenShrestha980 <s.bijen93@gmail.com>
2025-01-01 18:51:58 +0545

    feat: column drag event

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,10 +1,33 @@
+import { createPortal } from "react-dom";
+import { useMemo, useState } from "react";
 import { Plus } from "lucide-react";
-import { useState } from "react";
+import { arrayMove, SortableContext } from "@dnd-kit/sortable";
+import {
+  DndContext,
+  DragEndEvent,
+  DragOverlay,
+  DragStartEvent,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
 import { Column, Id } from "../types";
 import ColumnContainer from "./ColumnContainer";
 
 const Board = () => {
   const [columns, setColumns] = useState<Column[]>([]);
+  const [activeColumn, setActiveColumn] = useState<Column | null>(null);
+  const columnsId = useMemo(
+    () => columns.map((column) => column.id),
+    [columns]
+  );
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 4,
+      },
+    })
+  );
 
   const createNewColumn = () => {
     const columnToAdd: Column = {
@@ -19,29 +42,81 @@ const Board = () => {
     setColumns(newColumns);
   };
 
+  const onDragStart = (e: DragStartEvent) => {
+    if (e.active.data.current?.type === "Column") {
+      setActiveColumn(e.active.data.current.column);
+      return;
+    }
+  };
+
+  const onDragEnd = (e: DragEndEvent) => {
+    const { active, over } = e;
+    if (!over) {
+      return;
+    }
+
+    const activeColumnId = active.id;
+    const overColumnId = over.id;
+
+    if (activeColumnId === overColumnId) {
+      return;
+    }
+
+    setColumns((columns) => {
+      const activeColumnIndex = columns.findIndex(
+        (column) => column.id === activeColumnId
+      );
+      const overColumnIndex = columns.findIndex(
+        (column) => column.id === overColumnId
+      );
+
+      return arrayMove(columns, activeColumnIndex, overColumnIndex);
+    });
+  };
+
   const generateId = () => {
     return Math.floor(Math.random() * 1000000);
   };
+
   return (
     <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
-      <div className="m-auto flex gap-4">
-        <div className="flex gap-4">
-          {columns.map((column) => (
-            <ColumnContainer
-              key={column.id}
-              column={column}
-              deleteColumn={deleteColumn}
-            />
-          ))}
+      <DndContext
+        sensors={sensors}
+        onDragStart={onDragStart}
+        onDragEnd={onDragEnd}
+      >
+        <div className="m-auto flex gap-4">
+          <div className="flex gap-4">
+            <SortableContext items={columnsId}>
+              {columns.map((column) => (
+                <ColumnContainer
+                  key={column.id}
+                  column={column}
+                  deleteColumn={deleteColumn}
+                />
+              ))}
+            </SortableContext>
+          </div>
+          <button
+            onClick={() => createNewColumn()}
+            className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mianBackground border-2 border-columnBackground p-4 ring-rose-500 hover:ring-2 flex gap-2"
+          >
+            <Plus />
+            Add Column
+          </button>
         </div>
-        <button
-          onClick={() => createNewColumn()}
-          className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mianBackground border-2 border-columnBackground p-4 ring-rose-500 hover:ring-2 flex gap-2"
-        >
-          <Plus />
-          Add Column
-        </button>
-      </div>
+        {createPortal(
+          <DragOverlay>
+            {activeColumn && (
+              <ColumnContainer
+                column={activeColumn}
+                deleteColumn={deleteColumn}
+              />
+            )}
+          </DragOverlay>,
+          document.body
+        )}
+      </DndContext>
     </div>
   );
 };
--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -1,5 +1,7 @@
 import { Trash2 } from "lucide-react";
 import { Column, Id } from "../types";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 interface Props {
   column: Column;
@@ -7,9 +9,47 @@ interface Props {
 }
 const ColumnContainer = (props: Props) => {
   const { column, deleteColumn } = props;
+
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: column.id,
+    data: {
+      type: "Column",
+      column,
+    },
+  });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  if (isDragging) {
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        className="bg-slate-100 opacity-40 border-2 border-rose-500 w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col"
+      />
+    );
+  }
   return (
-    <div className="bg-slate-100 w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col">
-      <div className="bg-slate-200 text-md h-[60px] cursor-grab rounded-md rounded-b-none p-3 border-slate-100 border-4 flex items-center justify-between">
+    <div
+      ref={setNodeRef}
+      style={style}
+      className="bg-slate-100 w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col"
+    >
+      <div
+        {...attributes}
+        {...listeners}
+        className="bg-slate-200 text-md h-[60px] cursor-grab rounded-md rounded-b-none p-3 border-slate-100 border-4 flex items-center justify-between"
+      >
         <div className="flex gap-2">
           <div className="flex justify-center items-center bg-slate-100 px-2 py-1 text-sm rounded-md">
             0
